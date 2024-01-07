7e2496c9c9d98c02604f8c7c3ad83bdd8eb9ae4f
Italo Bruno Garcia Cabral <g1brunoyokima@hotmail.com>
2024-01-06 22:03:19 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+//import package and separated inside modules
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+
+const PORT = process.env.PORT || 8000
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
