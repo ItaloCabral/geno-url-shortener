57387585fd4d5f31cb131706195d695ab23d921a
Italo Bruno Garcia Cabral <g1brunoyokima@hotmail.com>
2024-01-08 05:08:27 -0300

    feat(link): short link generation use case

---
+++ src/use-cases/generate-short-link.ts
@@ -0,0 +1,16 @@
+/**
+ * TODO: Implement user id  
+ */
+
+type GenerateShortLinkRequest = {
+    url: string;
+}
+
+type GenerateShortLinkResponse = {}
+
+export class GenerateShortLink {
+
+    async execute(url: string) {
+    }
+
+}
