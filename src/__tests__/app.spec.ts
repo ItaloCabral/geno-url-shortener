ff6e99dd8617badc8106ef60a24665a350a9f9f6
SouzaGiovanna <gi.souza03@gmail.com>
2025-02-10 19:20:11 -0300

    test(app): health check route test

--- src/__tests__/app.spec.ts
+++ src/__tests__/app.spec.ts
@@ -8,8 +8,8 @@ const app = App
 const requestWithSupertest = supertest(app)
 
 describe('Debug endpoints', () => {
-    it('GET /debug should return a message', async () => {
-        const response = await requestWithSupertest.get('/debug')
+    it('GET /check should return a message', async () => {
+        const response = await requestWithSupertest.get('/check')
         expect(response.status).toBe(200)
         expect(response.body).toEqual({ message: 'Up and running!' })
     })
