cde17b775e25525917f95cd1d3ed88180d2282c5
SouzaGiovanna <gi.souza03@gmail.com>
2025-02-10 19:19:54 -0300

    chore: remove unused routes file

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const routes = Router();
-
-routes.get('/debug', (request: Request, response: Response) => {
-    return response.json({ message: 'Up and running!' });
-});
-
-export default routes;
